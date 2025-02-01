946a43e060d10d50f83d425ab7642a4d94d2a52c
Fly Man <fly.man.opensim@gmail.com>
2025-02-01 19:29:02 +0100

    Update start-corrade.sh

--- start-corrade.sh
+++ start-corrade.sh
@@ -5,16 +5,17 @@ cd /corrade
 
 # check if Configuration.xml exists, if yes, skip
 test -f Configuration.xml && {
-    echo "### Coniguration.xml exists. Keeping it."
+    echo "### Configuration.xml exists. Keeping it."
 } || {
     echo "### Generating Configuration.xml with values from environment variables"
     echo "### If Corrade does not start as expected, make sure you have all these variables set:"
-    echo "### FIRSTNAME, LASTNAME, PASSWORD, GROUP, GROUPPW"
+    echo "### FIRSTNAME, LASTNAME, PASSWORD, GRIDURL, GROUP, GROUPPW"
     PASSWORD=`echo -n "$PASSWORD" | md5sum | awk '{print $1}'`
     GROUPPW=`echo -n "$GROUPPW" | sha1sum | awk '{print $1}'`
     sed -i "s/##FIRSTNAME##/$FIRSTNAME/" Configuration.xml.template
     sed -i "s/##LASTNAME##/$LASTNAME/" Configuration.xml.template
     sed -i "s/##PASSWORD##/$PASSWORD/" Configuration.xml.template
+    sed -i "S/##GRIDURL##/$GRIDURL/" Configuration.xml.template
     sed -i "s/##GROUP##/$GROUP/" Configuration.xml.template
     sed -i "s/##GROUPPW##/$GROUPPW/" Configuration.xml.template
     mv Configuration.xml.template Configuration.xml
@@ -23,5 +24,5 @@ test -f Configuration.xml && {
     cp Nucleus.xml NucleusConfiguration.xml
 }
 
-
+# Start Corrade
 ./Corrade
